f3ad566627b11cc3c12ea079eb97aa0b0f681f64
whdjh <whdjh98@naver.com>
2024-03-07 10:30:15 +0900

    [FE]게시판 레이아웃 구성

---
+++ everytime/src/routes/BoardList.js
@@ -0,0 +1,14 @@
+// 리액트에서 이동한 페이지 내용을 출력하기위해 리액트 라우터를 설치
+// 프로젝트 fe디렉토리에서 다음 명령어로 리액트 라우터 설치
+// npm install react-router-dom
+import React from 'react';
+
+const BoardList = () => {
+	return (
+		<div>
+			게시판 목록
+		</div>
+	);
+};
+
+export default BoardList;
